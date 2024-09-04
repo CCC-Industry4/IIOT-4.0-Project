fed35ad4f8ff8664a5a263fd4002d30e43f478ea
Julian Laxamana <julianlaxamana123@gmail.com>
2024-09-04 12:32:15 -0700

    Added Raspberry Pi Camera Support

--- start.sh
+++ start.sh
@@ -2,8 +2,9 @@
 #This script starts all necessary services to run the project along with the GUI
 
 mosquitto&
-
-cd /usr/local/ignition && sudo ./ignition.sh start
+node-red
+export DISPLAY=:1 && xinput set-prop 11 155 -1 0 1 0 -1 1 0 0 1
+cd /usr/local/ignition && sudo ./ignition.sh start && ./node-red
 
 #cd ~/IIOT-4.0-Project/GUI && ./IOT_GUI
 
